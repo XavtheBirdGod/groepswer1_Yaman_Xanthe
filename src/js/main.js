4714eb6e5c22f16a95cefb1294aeadfb5cf75e3e
Xanthe Vanneste Deloose <xanthe.vannestedeloose@gmail.com>
2025-11-20 14:12:56 +0100

    countrylist

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,14 @@
 import '../scss/styles.scss';
 
 // Import all of Bootstrap’s JS
+
+import{showCountries} from "./components/countryList.js";
+
 import * as bootstrap from 'bootstrap';
 
 //eigen js
+
+
+document.addEventListener("DOMContentLoaded", ()=>{
+    showCountries()
+})
